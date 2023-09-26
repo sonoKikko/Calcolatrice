eec11d8b042ef5a9b54208d7681b0b22a8f75e85
sonoKikko <f.venuto@studenti.unisa.it>
2023-09-26 12:22:43 +0200

    Upload Project

---
+++ src/main/java/it/unisa/venuto/Calcolatrice.java
@@ -0,0 +1,22 @@
+package it.unisa.venuto;
+
+public class Calcolatrice {
+    private int a,b;
+
+    public Calcolatrice(){
+        this.a= 0;
+        this.b= 0;
+    }
+
+    public int somma(int a, int b){
+
+        return a+b;
+    }
+
+    public int moltiplicazione(int a, int b){
+
+        return a*b;
+    }
+
+}
+
